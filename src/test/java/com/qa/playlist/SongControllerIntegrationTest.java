ef826f2e6821de237f98e05e2247e702f0e82422
akakech <j.b.iahunanya@outlook.com>
2021-12-17 05:00:01 +0000

    CNP-21 Added schema and data files for testing, also added getById and getAll tests

--- src/test/java/com/qa/playlist/SongControllerIntegrationTest.java
+++ src/test/java/com/qa/playlist/SongControllerIntegrationTest.java
@@ -1,8 +1,12 @@
 package com.qa.playlist;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +14,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)//Use Random Port to avoid collisions.
 @AutoConfigureMockMvc
+
+@Sql(scripts = { "classpath:playlist-schema.sql",
+"classpath:playlist.data.sql" }, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+
 public class SongControllerIntegrationTest {
 	
 	
@@ -40,5 +49,42 @@ public class SongControllerIntegrationTest {
 		
 		this.mvc.perform(req).andExpect(statusFlag).andExpect(bodyCheck);
 	}
+	
+	@Test
+	void testGetById() throws Exception{
+		RequestBuilder req = get("/get/1");
+		
+		Song testGetSong = new Song(1, "Le Temps", "Fleur Floride", "Tayc");
+		//String songToJSON = this.mapper.writeValueAsString(testGetSong);
+		String songToJSON2 = this.mapper.writeValueAsString(new Song(1, "Le Temps", "Fleur Floride", "Tayc"));
+		
+		
+		ResultMatcher statusFlag = status().isOk();
+		ResultMatcher bodyCheck = content().json(songToJSON2);
+		
+		this.mvc.perform(req).andExpect(statusFlag).andExpect(bodyCheck);
+	}
+	
+	@Test
+	void testGetAll() throws Exception{
+		RequestBuilder req = get("/getAll");
+		
+		List<Song> testGetSongs = List.of(new Song(1, "Le Temps", "Fleur Floride", "Tayc"),
+				new Song(2, "Set Me Alight", "Sempre Verao", "Nelson Freitas"),
+				new Song(3, "Essence", "Made In Lagos", "WizKid"));
+		
+		String listsToJSON = this.mapper.writeValueAsString(testGetSongs);
+		
+		ResultMatcher statusFlag = status().isOk();
+		ResultMatcher bodyCheck = content().json(listsToJSON);
+		
+		this.mvc.perform(req).andExpect(statusFlag).andExpect(bodyCheck);
+	}
+	
+	@Test
+	void testDelete() throws Exception {
+		this.mvc.perform(delete("/deleteSong/1")).andExpect(status().isNoContent());
+	}
+	
 	
 }
